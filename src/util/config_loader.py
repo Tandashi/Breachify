46a880c787119ba9843c49ce2ab62d9a95312d4a
Tandashi <18377875+Tandashi@users.noreply.github.com>
2020-12-25 00:41:32 +0100

    doc: Add method doct and comments

--- src/util/config_loader.py
+++ src/util/config_loader.py
@@ -2,5 +2,10 @@ import yaml
 
 class ConfigLoader:
   def load_config():
+    """
+    Load the configuration from the config.yaml
+
+    returns: The parsed content of the config.yaml as dict
+    """
     with open('config.yaml', 'r') as f:
       return yaml.load(f, Loader=yaml.FullLoader)
